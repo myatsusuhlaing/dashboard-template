8008ed8ec42861b79609b12d97b5b9371c8c04b6
myatsusuhlaing <myatsusuhlaing04@gmail.com>
2023-05-12 15:40:35 +0630

    Login Api

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,45 +1,75 @@
-import  React from 'react';
+import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import axios from 'axios';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    axios.post('http://121.54.167.132:8888/api/user/login', {
+      email,
+      password,
+    })
+      .then((response) => {
+        console.log(response.data);
+        if (response.status === 200) {
+          window.location.href = '/';
+        } else {
+          alert(response.data.message);
+        }
+        setPassword('');
+        setEmail('');
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    };
+
   return (
     <>
       <CssBaseline />
       <div className=' h-screen flex justify-center w-full md:w-auto items-center'>
         <div className='border border-slate-500 rounded-lg sm:w-4/12 p-5 mx-auto flex flex-col'>
-            <h2 className='text-xl text-slate-700'>
-                Login In
-                <hr className=" divide-y mt-2 border-2 border-slate-400"></hr>
-            </h2>
-            <Box
-                component="form"
-                paddingTop={3}
-                sx={{
-                    '& > :not(style)': { m: 1, width: '28ch' },
-                }}
-                noValidate
-                autoComplete="off"
-            />
-            <TextField 
-                id="standard-basic"  
-                label="Username" 
-                variant="standard" 
-                sx={{paddingBottom :2 }}
-            />
-            <TextField
-                id="filled-password-input"
-                label="Password"
-                type="password"
-                autoComplete="current-password"
-                variant="standard"
-            />
-            <Stack spacing={2} paddingTop={4}>
-                <Button variant="contained">Login</Button>
-            </Stack>
+          <h2 className='text-xl text-slate-700'>
+            Log In
+            <hr className=" divide-y mt-2 border-2 border-slate-400"></hr>
+          </h2>
+          <Box
+            component="form"
+            paddingTop={3}
+            sx={{
+              '& > :not(style)': { m: 1, width: '28ch' },
+            }}
+            noValidate
+            autoComplete="off"
+          />
+          <TextField
+            id="standard-basic"
+            label="Username"
+            variant="standard"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            required
+            sx={{ paddingBottom: 2 }}
+          />
+          <TextField
+            id="filled-password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            variant="standard"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            required
+          />
+          <Stack spacing={2} paddingTop={4}>
+            <Button variant="contained" onClick={handleLogin}>Log In</Button>
+          </Stack>
         </div>
       </div>
     </>
