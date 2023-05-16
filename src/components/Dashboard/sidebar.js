b4923e1474f8ad8f9a592c0b4417ed45c338c1ba
myatsusuhlaing <myatsusuhlaing04@gmail.com>
2023-05-17 00:05:06 +0630

    Changing sidebar drawer design

--- src/components/Dashboard/sidebar.js
+++ src/components/Dashboard/sidebar.js
@@ -1,14 +1,8 @@
-import React, { useState } from "react";
-import { styled, useTheme } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import MuiDrawer from "@mui/material/Drawer";
-import List from "@mui/material/List";
-import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import React from "react";
 import ListItem from "@mui/material/ListItem";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
@@ -17,219 +11,90 @@ import NearMeIcon from "@mui/icons-material/NearMe";
 import SettingsIcon from "@mui/icons-material/Settings";
 import { useNavigate } from "react-router-dom";
 import { useAppStore } from "../../AppStore";
-
-const drawerWidth = 240;
-
-const openedMixin = (theme) => ({
-  width: drawerWidth,
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: "hidden",
-});
-
-const closedMixin = (theme) => ({
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: "hidden",
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up("sm")]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
-});
-
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "flex-end",
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-}));
-
-const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  width: drawerWidth,
-  flexShrink: 0,
-  whiteSpace: "nowrap",
-  boxSizing: "border-box",
-  ...(open && {
-    ...openedMixin(theme),
-    "& .MuiDrawer-paper": openedMixin(theme),
-  }),
-  ...(!open && {
-    ...closedMixin(theme),
-    "& .MuiDrawer-paper": closedMixin(theme),
-  }),
-}));
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 
 export default function Sidebar() {
-  const theme = useTheme();
-  // const [open, setOpen] = useState(false);
   const updateOpen = useAppStore((state) => state.updateOpen);
   const open = useAppStore((state) => state.dopen);
   const navigate = useNavigate();
+  
+  return (  
+    <SwipeableDrawer
+      variant="temporary"
+      anchor="left"
+      open={open}
+      onOpen={() => updateOpen(true)}
+      onClose={() => updateOpen(false)}
+    >
+  <Box sx={{ minHeight :60 ,px :15}}/>
+    <List>
+      {/* ----------------------------------- Dashboard ----------------------------------- */}
+      <ListItem
+        disablePadding
+        onClick={() => {
+          navigate("/");
+          updateOpen(false);
+        }}
+      >
+        <ListItemButton
+          sx={{
+            minHeight: 48,
+            justifyContent: open ? "initial" : "center",
+            px: 2.5,
+          }}
+        >
+          <ListItemIcon sx={{ justifyContent: "center" }}>
+            <DashboardIcon />
+          </ListItemIcon>
+          <ListItemText primary="Dashboard" sx={{ opacity: open ? 1 : 0 }} />
+        </ListItemButton>
+      </ListItem>
 
-  // const MENU_LIST = [
-  //   {
-  //     id: 1,
-  //     name: "Dashboard",
-  //     icon: <DashboardIcon />,
-  //     route: "/",
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "Menu1",
-  //     icon: <DashboardIcon />,
-  //     route: "/Map",
-  //   },
-  // ];
+      {/* --------------------------------- Map --------------------------------- */}
+      <ListItem
+        disablePadding
+        onClick={() => {
+          navigate("/Map");
+          updateOpen(false);
+        }}
+      >
+        <ListItemButton
+          sx={{
+            minHeight: 48,
+            justifyContent: open ? "initial" : "center",
+            px: 2.5,
+          }}
+        >
+          <ListItemIcon sx={{ justifyContent: "center" }}>
+            <NearMeIcon />
+          </ListItemIcon>
+          <ListItemText primary="Map" sx={{ opacity: open ? 1 : 0 }} />
+        </ListItemButton>
+      </ListItem>
 
-  return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <Box height={50} />
-      <Drawer variant="permanent" open={open}>
-        <DrawerHeader>
-          <IconButton>
-            {theme.direction === "rtl" ? (
-              <ChevronRightIcon />
-            ) : (
-              <ChevronLeftIcon />
-            )}
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-
-        <List>
-          {/* {MENU_LIST.map((menu) => (
-            <React.Fragment key={menu.id}>
-              <ListItem
-                disablePadding
-                sx={{ display: "block" }}
-                onClick={() => {
-                  navigate(menu.route);
-                }}
-              >
-                <ListItemButton
-                  sx={{
-                    minHeight: 48,
-                    justifyContent: open ? "initial" : "center",
-                    px: 2.5,
-                  }}
-                >
-                  <ListItemIcon
-                    sx={{
-                      minWidth: 0,
-                      mr: open ? 3 : "auto",
-                      justifyContent: "center",
-                    }}
-                  >
-                    
-                    <DashboardIcon />
-                  </ListItemIcon>
-                  <ListItemText
-                    primary={menu.name}
-                    sx={{ opacity: open ? 1 : 0 }}
-                  />
-                </ListItemButton>
-              </ListItem>
-            </React.Fragment>
-          ))} */}
-
-          {/* ----------------------------------- Dashboard ----------------------------------- */}
-          <ListItem
-            disablePadding
-            sx={{ display: "block" }}
-            onClick={() => {
-              navigate("/");
-            }}
-            >
-            <ListItemButton
-              sx={{
-                minHeight: 48,
-                justifyContent: open ? "initial" : "center",
-                px: 2.5,
-              }}
-              >
-              <ListItemIcon
-                sx={{
-                  minWidth: 0,
-                  mr: open ? 3 : "auto",
-                  justifyContent: "center",
-                }}
-                >
-                <DashboardIcon />
-              </ListItemIcon>
-              <ListItemText
-                primary="Dashboard"
-                sx={{ opacity: open ? 1 : 0 }}
-              />
-            </ListItemButton>
-          </ListItem>
-
-          {/* --------------------------------- Map --------------------------------- */}
-          <ListItem
-            disablePadding
-            sx={{ display: "block" }}
-            onClick={() => {
-              navigate("/Map");
-            }}
-            >
-            <ListItemButton
-              sx={{
-                minHeight: 48,
-                justifyContent: open ? "initial" : "center",
-                px: 2.5,
-              }}
-              >
-              <ListItemIcon
-                sx={{
-                  minWidth: 0,
-                  mr: open ? 3 : "auto",
-                  justifyContent: "center",
-                }}
-                >
-                <NearMeIcon />
-              </ListItemIcon>
-            <ListItemText primary="Map" sx={{ opacity: open ? 1 : 0 }} />
-            </ListItemButton>
-          </ListItem>
-
-          {/* ------------------------------------- Settings ---------------------------------- */}
-          <ListItem
-            disablePadding
-            sx={{ display: "block" }}
-            onClick={() => {
-              navigate("/Settings");
-            }}
-            >
-            <ListItemButton
-              sx={{
-                minHeight: 48,
-                justifyContent: open ? "initial" : "center",
-                px: 2.5,
-              }}
-              >
-              <ListItemIcon
-                sx={{
-                  minWidth: 0,
-                  mr: open ? 3 : "auto",
-                  justifyContent: "center",
-                }}
-                >
-                <SettingsIcon />
-              </ListItemIcon>
-              <ListItemText primary="Settings" sx={{ opacity: open ? 1 : 0 }} />
-            </ListItemButton>
-          </ListItem>
-        </List>
-        <Divider/>
-    </Drawer>
-  </Box>
+      {/* --------------------------------- Settings --------------------------------- */}
+      <ListItem
+        disablePadding
+        onClick={() => {
+          navigate("/Settings");
+          updateOpen(false);
+        }}
+      >
+        <ListItemButton
+          sx={{
+            minHeight: 48,
+            justifyContent: open ? "initial" : "center",
+            px: 2.5,
+          }}
+        >
+          <ListItemIcon sx={{ justifyContent: "center" }}>
+            <SettingsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Settings" sx={{ opacity: open ? 1 : 0 }} />
+        </ListItemButton>
+      </ListItem>
+    </List>
+    <Divider/>
+  </SwipeableDrawer>
   );
 }
